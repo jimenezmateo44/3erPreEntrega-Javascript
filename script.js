e1f16edb5ad812cfd289f26090f4d24c316bf39e
Mateo Jimenez <mateojimenez37@gmail.com>
2023-08-25 18:30:59 -0300

    -- nuevos productos --

--- script.js
+++ script.js
@@ -47,6 +47,30 @@ const productos = [
         stock: 5,
         categoria: 'Buzos'
     },
+    {
+        id: 7,
+        nombre: 'Piluso Adidas Classic',
+        precio: 19000,
+        imagen: 'pilusoadidas.jpeg',
+        stock: 5,
+        categoria: 'Accesorios'
+    },
+    {
+        id: 8,
+        nombre: 'Buzo Puma Street',
+        precio: 52000,
+        imagen: 'buzopuma.jpeg',
+        stock: 5,
+        categoria: 'Buzos'
+    },
+    {
+        id: 9,
+        nombre: 'Riñonera Jordan',
+        precio: 23000,
+        imagen: 'rinonerajordan.jpeg',
+        stock: 5,
+        categoria: 'Accesorios'
+    },
 ];
 
 //renderizado de productos
@@ -62,7 +86,7 @@ productos.forEach(producto => {
    <p>$${producto.precio}</p>
    <div class="btnTarjeta">
    <a href="#" class="btnAgregarCarrito">Añadir al carrito</a>
-   <a href="#" class="agregarFavoritos"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+   <a href="#" class="agregarFavoritos"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="black" class="bi bi-heart" viewBox="0 0 16 16">
    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
  </svg></a>
     </div>
